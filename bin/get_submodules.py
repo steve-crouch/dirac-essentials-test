3d5391bde1a70427a28d8ebba44e0f2f378069ec
Pip Grylls <pipgryllsastro@gmail.com>
2021-11-15 13:39:35 +0000

    debug

--- bin/get_submodules.py
+++ bin/get_submodules.py
@@ -13,7 +13,7 @@ os.system('ls')
 with open('_config.yml') as config:
     data = load(config, Loader=Loader)
     for episode_info in data['collections']:
-        if data['collections'][episode_info].get('type', False) != 'episode':
+        if data['collections'][episode_info].get('type', None) == 'episode':
             episode_name = data['collections'][episode_info].get('label', None)
             gh_branch = data['collections'][episode_info].get('branch', 'gh-pages')
             command = f"git submodule add -b {gh_branch} https://github.com/Southampton-RSG-Training/{episode_name}.git collections/_{episode_name}"
