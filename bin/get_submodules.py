b828ac6a018039aee6c8b7bd3be11acc8aac9062
Pip Grylls <pipgryllsastro@gmail.com>
2021-11-23 14:18:26 +0000

    restucturing files during gh-aactions to allow for lesson build

--- bin/get_submodules.py
+++ bin/get_submodules.py
@@ -15,28 +15,35 @@ os.system("mkdir -p submodules")
 
 with open('_data/lessons.yml') as config:
     data = load(config, Loader=Loader)
-    for episode_info in data['lessons']:
-        if episode_info.get('type', None) == 'episode':
+    for lesson_info in data['lessons']:
+        if lesson_info.get('type', None) == 'episode':
             # Create the command to pull the subdirectory from GitHub
-            episode_name = episode_info.get('gh-name', None)
-            gh_branch = episode_info.get('branch', 'gh-pages')
-            command = f"git submodule add --force -b {gh_branch} https://github.com/Southampton-RSG-Training/{episode_name}.git submodules/{episode_name}"
+            lesson_name = lesson_info.get('gh-name', None)
+            gh_branch = lesson_info.get('branch', 'gh-pages')
+            command = f"git submodule add --force -b {gh_branch} https://github.com/Southampton-RSG-Training/{lesson_name}.git submodules/{lesson_name}"
             os.system(command)
             os.system("git submodule update --remote --merge")
-            # move required files from the subdirectories to _includes/rsg/{episode_name}/...
-            # make directory
-            dest = f"_includes/rsg/{episode_name}"
-            os.system(f"mkdir -p {dest}")
-            for directory in ["_episodes", "_episodes_rmd", "fig"]:
-                os.system(f"cp -r submodules/{episode_name}/{directory} {dest}/{directory}")
-            for file in ["index.md", "setup.md", "_includes/rsg/schedule.html", "reference.md"]:
-                os.system(f"cp submodules/{episode_name}/{file} {dest}/{file.split('/')[-1]}")
+            # move required files from the subdirectories to _includes/rsg/{lesson_name}/...
 
+            # make directory
+            dest = f"_includes/rsg/{lesson_name}"
+            os.system(f"mkdir -p {dest}")
+            for file in ["setup.md", "_includes/rsg/schedule.html"]:
+                os.system(f"cp submodules/{lesson_name}/{file} {dest}/{file.split('/')[-1]}")
+
+            for directory in ["_episodes", "_episodes_rmd"]:
+                dest = f"collections/{directory}/{lesson_name}"
+                os.system(f"mkdir -p {dest}")
+                os.system(f"cp -r submodules/{lesson_name}/{directory}/ {dest}/")
+            for file in ["reference.md"]:
+                dest = f"collections/_episodes/{lesson_name}"
+                os.system(f"mkdir -p {dest}")
+                os.system(f"cp submodules/{lesson_name}/{file} {dest}/{file.split('/')[-1]}")
             # Copy the figures from submodule into fig
-            os.system(f"cp -r submodules/{episode_name}/fig/ fig/")
+            os.system(f"cp -r submodules/{lesson_name}/fig/ fig/")
             # If the lesson has assoiated slides
             try:
-                os.system(f"cp -r submodules/{episode_name}/slides ./")
+                os.system(f"cp -r submodules/{lesson_name}/slides ./")
             except:
-                print(f"{episode_name} has no slides")
+                print(f"{lesson_name} has no slides")
 
