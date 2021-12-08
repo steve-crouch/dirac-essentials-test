9a4033364becf0d45f4bcbf08b863afb66777b44
Edward Parkinson <e.parkinson@soton.ac.uk>
2021-12-08 15:03:57 +0000

    get_schedules.py can now handle multiple dates/start-times

--- bin/get_schedules.py
+++ bin/get_schedules.py
@@ -23,10 +23,10 @@ with open("_data/lessons.yml", "r") as f:
 
 for lesson in lesson_config["lessons"]:
 
-    lesson_date = lesson.get("date", None)
     lesson_title = lesson.get("title", None)
     lesson_name = lesson.get("gh-name", None)
-    lesson_start_time = lesson.get("start-time", None)
+    lesson_date = lesson.get("date", None)              # can be a list
+    lesson_start_time = lesson.get("start-time", None)  # can be a list
 
     if [thing for thing in (lesson_name, lesson_date, lesson_title, lesson_start_time) if thing is None]:
         raise ValueError(f"gh-name, date, title, and start-time are required for each lesson")
@@ -39,24 +39,36 @@ for lesson in lesson_config["lessons"]:
     schedule_tables = pandas.read_html(schedule_html, flavor="bs4")  # reads in multiple tables, for multi-day lessons
     html_soup = bs(schedule_html, "html.parser")  # maybe this could be more efficient? :)
 
+    # Change things to a list, if they are not already so we can loop over them
+
+    if type(lesson_date) is not list:
+        lesson_date = [lesson_date]
+    if type(lesson_start_time) is not list:
+        lesson_start_time = [lesson_start_time]
+
+    # Loop over each schedule table, if the event is multi-day
+
     for i, schedule_table in enumerate(schedule_tables):
         schedule_table.columns = ["time", "session"]
         lesson_permalink = html_soup.find_all("a", href=True)[i]["href"]  # assume each table has a link to the lesson
+
+        this_lesson_start_time = lesson_start_time[i]
+        this_lesson_date = lesson_date[i]
 
         # There is some sorcery required to mangle start-time into a datetime
         # object, depending on how it is written. Examples of the different
         # formats are inline with how we deal with them below.
 
-        if type(lesson_start_time) is str:
+        if type(this_lesson_start_time) is str:
             try:
-                start_time = datetime.datetime.strptime(lesson_start_time, "%I:%M %p")  # start-time: 9:30 am
+                start_time = datetime.datetime.strptime(this_lesson_start_time, "%I:%M %p")  # start-time: 9:30 am
             except ValueError:
-                start_time = datetime.datetime.strptime(lesson_start_time, "%H:%M")     # start-time: "9:30"
-        elif type(lesson_start_time) is int:
-            hours, minutes = divmod(lesson_start_time, 60)
+                start_time = datetime.datetime.strptime(this_lesson_start_time, "%H:%M")     # start-time: "9:30"
+        elif type(this_lesson_start_time) is int:
+            hours, minutes = divmod(this_lesson_start_time, 60)
             start_time = datetime.datetime.strptime(f"{hours}:{minutes}", "%H:%M")      # start-time: 9:30
         else:
-            raise ValueError(f"start-time {lesson_start_time} for lesson \"{lesson}\" is invalid format")
+            raise ValueError(f"start-time {this_lesson_start_time} for lesson \"{lesson['title']}\" is invalid format")
 
         # Calculate the time difference between the start time, and the start
         # time in the original schedule. This delta time (in minutes) is added
@@ -73,7 +85,7 @@ for lesson in lesson_config["lessons"]:
         <div class="row">
             <div class="col-md-6">
                 <a href="{lesson_permalink}"><h3>{lesson_title}</h3></a>
-                <h4>{lesson_date}</h4>
+                <h4>{this_lesson_date}</h4>
                 <table class="table table-striped">
         """
 
