98fac850404582dadbfec4942687756c03269338
Pip Grylls <pipgryllsastro@gmail.com>
2022-03-14 15:10:03 +0000

    adding favicon generation build will fail

--- bin/make_favicons.py
+++ bin/make_favicons.py
@@ -4,4 +4,4 @@ favicon_dir = "assets/favicons/rsg/"
 base_favicon = favicon_dir+"rsg_fav_base.png"
 
 with Favicons(base_favicon, favicon_dir) as favicons:
-    await favicons.generate()
+    favicons.generate()
